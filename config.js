942cdca704a64b0fdb4d3ca185c9d222e332afe7
ASohb <92811009+ASohb@users.noreply.github.com>
2023-02-14 17:41:20 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCoQlNBS639vLphbu0IAJwugI8IFic7g5k",
+  authDomain: "appnarracaodehistorias-49675.firebaseapp.com",
+  projectId: "appnarracaodehistorias-49675",
+  storageBucket: "appnarracaodehistorias-49675.appspot.com",
+  messagingSenderId: "360250733554",
+  appId: "1:360250733554:web:9b1a35679d078208e6f16b"
+};
+
